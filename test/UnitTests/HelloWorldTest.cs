c415afcfe9b3cbd8f54a6c82029251d841e78388
bgoldbeck <bgoldbeck21@gmail.com>
2018-07-05 09:41:43 -0700

    Adding basic dummy unit test for an example

---
+++ test/UnitTests/HelloWorldTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class HelloWorldTest
+    {
+        [TestMethod]
+        public void HelloWorld_GetHelloWorld_IsHelloWorld()
+        {
+            HelloWorld hello = new HelloWorld();
+            Assert.AreEqual(hello.Hello(), "Hello World!");
+            return;
+        }
+    }
+}
