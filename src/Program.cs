fe5b2dbd16258ac0df29052f18583a1bf45eabc1
bgoldbeck <bgoldbeck21@gmail.com>
2018-07-05 17:27:24 -0700

    Switching library to FluentFTP.dll

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,7 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Net;
-using System.Security.Cryptography;
-using System.Security.Permissions;
-using System.IO;
-using System.Security.Cryptography.X509Certificates;
-using Renci.SshNet;
-
+using FluentFTP;
 
 [ExcludeFromCodeCoverage]
 public class Program
@@ -18,35 +13,19 @@ public class Program
         Console.BackgroundColor = ConsoleColor.DarkMagenta;
         Console.ForegroundColor = ConsoleColor.Green;
 
-        // This is a code example from https://gist.github.com/piccaso/d963331dcbf20611b094#file-ssh-cs-L33
-        
-        // Setup Credentials and Server Information
-        ConnectionInfo ConnNfo = new ConnectionInfo("192.168.1.205", 22, "rando",
-            new AuthenticationMethod[]{
+        FtpClient client = new FtpClient("98.246.48.95");
+        client.Port = 21;
 
-                // Pasword based Authentication
-                new PasswordAuthenticationMethod("rando","all"),
+        // if you don't specify login credentials, we use the "anonymous" user account
+        client.Credentials = new NetworkCredential("", "");
 
-                // Key Based Authentication (using keys in OpenSSH Format)
-                //new PrivateKeyAuthenticationMethod("rando",new PrivateKeyFile[]{
-                //    new PrivateKeyFile(@"..\openssh.key","passphrase")
-                //}),
-            }
-        );
-        
-        // Execute a (SHELL) Command - prepare upload directory
-        using (var sshclient = new SshClient(ConnNfo))
-        {
-            sshclient.Connect();
-            Console.WriteLine(sshclient.CreateCommand("ls -lah").Execute());
-            sshclient.Disconnect();
-        }
+        // begin connecting to the server
+        client.Connect();
 
         bool running = true;
 
         while (running)
         {
-            //Console.Clear();
             Console.WriteLine("Hello World!");
             Console.ReadLine();
             running = false;
