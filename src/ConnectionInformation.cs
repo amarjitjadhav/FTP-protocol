246be7ae365d2dee12b9bfbca69cbc545da80637
bgoldbeck <bgoldbeck21@gmail.com>
2018-07-23 10:30:06 -0700

    Adding connection information class for storing and loading connections to the ftp server.

---
+++ src/ConnectionInformation.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace DumbFTP
+{
+    /// <summary>
+    /// Class that holds information about a particular connection.
+    /// </summary>
+    public class ConnectionInformation
+    {
+        public String Username { get; private set; } = "";
+        public String Password { get; private set; } = "";
+
+        public String ServerAddress { get; private set; } = "";
+
+        /// <summary>
+        /// Constructor to build a connection from information about the user passed in.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="serverAddress"></param>
+        public ConnectionInformation(String username, String password, String serverAddress)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.ServerAddress = serverAddress;
+        }
+        /// <summary>
+        /// Constructor that loads the connection information from a username, if
+        /// a user connection by that name has already been saved to a file.
+        /// </summary>
+        /// <param name="user"></param>
+        public ConnectionInformation(String username) => Load(username);
+
+        /// <summary>
+        /// Save the connection information to a file.
+        /// </summary>
+        public void Save()
+        {
+            String filepath = "saved_connections/" + Username + ".txt";
+
+            File.WriteAllText(filepath, String.Empty);
+
+            if (!Directory.Exists("saved_connections"))
+            {
+                Directory.CreateDirectory("saved_connections");
+            }
+
+            StreamWriter writer = new StreamWriter(File.Open(filepath, System.IO.FileMode.Create))
+            {
+                AutoFlush = true
+            };
+
+            writer.WriteLine("{0},{1},{2}", Username, Password, ServerAddress);
+            writer.Close();
+            return;
+        }
+
+        /// <summary>
+        /// Load the connection information from a file by the username.
+        /// </summary>
+        /// <param name="username"></param>
+        public void Load(String username)
+        {
+            String filepath = "saved_connections/" + username + ".txt";
+
+            StreamReader reader = new StreamReader(File.Open(filepath, System.IO.FileMode.Open));
+            String line = reader.ReadLine();
+            reader.Close();
+
+            String[] tokens = line.Split(',');
+            if (tokens != null && tokens.Length == 3)
+            {
+                this.Username = tokens[0];
+                this.Password = tokens[1];
+                this.ServerAddress = tokens[2];
+            }
+            
+            return;
+        }
+    }
+}
---
+++ test/XIntegrationTests/ConnectionInformationTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xunit;
+using Actions;
+using System.Collections.Generic;
+using System.IO;
+using DumbFTP;
+
+namespace XIntegrationTests
+{
+    public class ConnectionInformationTests
+    {
+
+        [Fact]
+        public void SaveConnectionInformation_FileExists()
+        {
+            ConnectionInformation connInfo = new ConnectionInformation("cs410", "cs410", "hypersweet.com");
+            connInfo.Save();
+
+            Assert.True(File.Exists("saved_connections/" + connInfo.Username + ".txt"));
+
+            File.Delete("saved_connections/" + connInfo.Username + ".txt");
+            return;
+        }
+
+        [Fact]
+
+        public void SaveConnectionInformation_LoadInformationMatches()
+        {
+            String user = "cs410";
+            String pass = "cs410";
+            String server = "hypersweet.com";
+
+            ConnectionInformation connInfo = new ConnectionInformation(user, pass, server);
+            connInfo.Save();
+
+            ConnectionInformation loadedConnInfo = new ConnectionInformation(user);
+
+            File.Delete("saved_connections/" + connInfo.Username + ".txt");
+
+            Assert.True(
+                loadedConnInfo.Username == user && 
+                loadedConnInfo.Password == pass && 
+                loadedConnInfo.ServerAddress == server);
+            return;
+        }
+
+    }
+}
