932f73652f80dd878df1c8f5b0b0b9f2c79020e5
bgoldbeck <bgoldbeck21@gmail.com>
2018-07-23 12:11:28 -0700

    Changed some control flow of the main program loop.

--- src/Program.cs
+++ src/Program.cs
@@ -34,75 +34,85 @@ class Program
 
         Browser browser = new Browser();
         double dt = 0;
+        ConsoleUI.ClearBuffers();
+
         while (running)
         {
             dt += Time.deltaMs;
-
-            ConsoleUI.WriteLine(dt.ToString() + " milliseconds have passed", Color.Salmon);
+            ConsoleKeyInfo input = new ConsoleKeyInfo();
 
             String clientContextState = Client.state == ClientState.VIEWING_LOCAL ? "Viewing Local" : "Viewing Remote";
 
             ConsoleUI.WriteLine("DumpFTP - " + clientContextState, Color.Gold);
             ConsoleUI.WriteLine(" Version - " + version, Color.Olive);
 
-            
+
             browser.DrawActionsMenu();
-            ConsoleKeyInfo input = ConsoleUI.ReadKey();
-            if (ConsoleUI.AnyKey())
+            ConsoleUI.Render();
+
+            while (!ConsoleUI.AnyKey())
             {
-                if (input.Key == ConsoleKey.Escape)
+                Time.Update();
+                dt += Time.deltaMs;
+                ConsoleUI.Write(0, 0, dt.ToString() + " milliseconds have passed", Color.Salmon);
+                input = ConsoleUI.ReadKey();
+                ConsoleUI.Render();
+            }
+
+            
+            if (input.Key == ConsoleKey.Escape)
+            {
+                // Exit program.
+                break;
+            }
+
+            foreach (IDFtpUI action in browser.Actions)
+            {
+                if (action.Key != input.Key)
                 {
-                    // Exit program.
-                    break;
+                    continue;
+                }
+                if (action.RequiresLogin && Client.ftpClient == null)
+                {
+                    // DO LOGIN.???
+                    continue;
+                }
+                if (action.RequiresFile && (Client.localSelection == null || Client.localSelection.Type() != FtpFileSystemObjectType.File))
+                {
+                    continue;
+                }
+                if (action.HideForDirectory && Client.localSelection != null && Client.localSelection.Type() == FtpFileSystemObjectType.Directory)
+                {
+                    continue;
+                }
+                if (action.HideForFile && Client.localSelection != null && Client.localSelection.Type() == FtpFileSystemObjectType.File)
+                {
+                    continue;
+                }
+                if (action.HideForLocal && Client.state == ClientState.VIEWING_LOCAL)
+                {
+                    continue;
+                }
+                if (action.HideForRemote && Client.state == ClientState.VIEWING_REMOTE)
+                {
+                    continue;
                 }
 
-                foreach (IDFtpUI action in browser.Actions)
+                // Run the action that was selected.
+                DFtpResult result = action.Go();
+
+                // If by running the action returned a list of file objects.
+                if (result is DFtpListResult)
                 {
-                    if (action.Key != input.Key)
-                    {
-                        continue;
-                    }
-                    if (action.RequiresLogin && Client.ftpClient == null)
-                    {
-                        // DO LOGIN.???
-                        continue;
-                    }
-                    if (action.RequiresFile && (Client.localSelection == null || Client.localSelection.Type() != FtpFileSystemObjectType.File))
-                    {
-                        continue;
-                    }
-                    if (action.HideForDirectory && Client.localSelection != null && Client.localSelection.Type() == FtpFileSystemObjectType.Directory)
-                    {
-                        continue;
-                    }
-                    if (action.HideForFile && Client.localSelection != null && Client.localSelection.Type() == FtpFileSystemObjectType.File)
-                    {
-                        continue;
-                    }
-                    if (action.HideForLocal && Client.state == ClientState.VIEWING_LOCAL)
-                    {
-                        continue;
-                    }
-                    if (action.HideForRemote && Client.state == ClientState.VIEWING_REMOTE)
-                    {
-                        continue;
-                    }
-
-                    // Run the action that was selected.
-                    DFtpResult result = action.Go();
-
-                    // If by running the action returned a list of file objects.
-                    if (result is DFtpListResult)
-                    {
-                        browser.DrawResultList((DFtpListResult)result);
-                    }
-                    else if (result.Type == DFtpResultType.Ok)
-                    {
-                        // Cool, we did the action.
-                        ConsoleUI.WriteLine("Action completed successfully", Color.Gold);
-                    }
+                    browser.DrawResultList((DFtpListResult)result);
+                }
+                else if (result.Type == DFtpResultType.Ok)
+                {
+                    // Cool, we did the action.
+                    ConsoleUI.WriteLine("Action completed successfully", Color.Gold);
                 }
             }
+            
 
             ConsoleUI.Render();
 
