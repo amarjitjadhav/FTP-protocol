df26fbfff623908bfc1aa99c9b7ea24c44bc6de7
amarjit <amarjit2@pdx.edu>
2018-07-31 00:06:26 -0700

    changes

--- src/DFtpResult.cs
+++ src/DFtpResult.cs
@@ -9,6 +9,7 @@ public class DFtpResult
 {
     public DFtpResultType Type { get; protected set; }
     public String Message { get; protected set; } = "";
+    public static object Result { get; internal set; }
 
     public DFtpResult(DFtpResultType type, String message = "")
     {
